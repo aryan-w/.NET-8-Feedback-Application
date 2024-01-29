df8abdf795131a5d3b27316247ece03534c9ab65
aryan-w <140028999+aryan-w@users.noreply.github.com>
2024-01-29 15:13:31 +0530

    Add files via upload

---
+++ DCandidate.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string fullName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+
+        public int age { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string gender { get; set; }
+
+        [Column(TypeName = "nvarchar(1000)")]
+        public string address { get; set; }
+    }
+}
